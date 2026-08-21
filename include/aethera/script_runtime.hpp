43bd10685e82190c4a267386fade6a1a744ae10c
Gyorinm <sarawahida111@gmail.com>
2026-08-21 02:50:14 +0100

    Add script runtime variables

--- include/aethera/script_runtime.hpp
+++ include/aethera/script_runtime.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "aethera/animation_graph.hpp"
 #include "aethera/behavior.hpp"
 #include "aethera/script.hpp"
@@ -10,10 +12,13 @@ class ScriptRuntime {
 public:
     void load(const ScriptProgram& program) { program_ = program; }
     bool run_trigger(const std::string& trigger, AnimationGraph& animation,
-                     BehaviorSystem& behavior) const;
+                     BehaviorSystem& behavior);
+    void set_variable(const std::string& name, ScriptValue value) { variables_[name] = std::move(value); }
+    const std::unordered_map<std::string, ScriptValue>& variables() const { return variables_; }
 
 private:
     ScriptProgram program_{};
+    std::unordered_map<std::string, ScriptValue> variables_;
 };
 
 } // namespace aethera
