09ac6ca0023a4960fa10358905a1aaeebd2d512e
Gyorinm <sarawahida111@gmail.com>
2026-08-21 02:49:46 +0100

    Expose script value parsing

--- include/aethera/script_expr.hpp
+++ include/aethera/script_expr.hpp
@@ -3,12 +3,10 @@
 #include <string>
 #include <unordered_map>
 #include <variant>
-#include <vector>
 
 namespace aethera {
 
 enum class CompareOp { Equal, NotEqual, Less, LessEqual, Greater, GreaterEqual };
-
 using ScriptValue = std::variant<float, bool, std::string>;
 
 struct ScriptExpression {
@@ -18,10 +16,10 @@ struct ScriptExpression {
     bool valid{false};
 };
 
+bool parse_script_value(const std::string& source, ScriptValue& out);
 bool evaluate_expression(const ScriptExpression& expression,
                          const std::unordered_map<std::string, ScriptValue>& variables,
                          bool& result);
-
 bool parse_expression(const std::string& source, ScriptExpression& expression);
 
 } // namespace aethera
