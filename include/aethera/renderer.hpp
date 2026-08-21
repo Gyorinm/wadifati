60f63bddc8639e3b1a56c9a96eeeab12423bbd54
Gyorinm <sarawahida111@gmail.com>
2026-08-21 04:33:49 +0100

    feat(aethera): expose reconstructed image rendering

--- include/aethera/renderer.hpp
+++ include/aethera/renderer.hpp
@@ -5,21 +5,26 @@
 #include "aethera/math.hpp"
 #include "aethera/object.hpp"
 #include "aethera/physics.hpp"
+#include "aethera/image_reconstruction.hpp"
 
 namespace aethera {
 
+class MeshTextureRenderer;
+
 class Renderer {
 public:
-    explicit Renderer(SDL_Renderer* renderer) : renderer_(renderer) {}
+    explicit Renderer(SDL_Renderer* renderer);
 
     void begin();
     void end();
     void line(Vec2 a, Vec2 b, float width = 2.0f);
     void circle(Vec2 center, float radius);
     void living_object(const LivingObject& object, const PhysicsWorld& physics);
+    void reconstructed_image(const ImageReconstructionResult& result);
 
 private:
     SDL_Renderer* renderer_{};
+    MeshTextureRenderer* mesh_renderer_{};
 };
 
 } // namespace aethera
