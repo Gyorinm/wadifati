8c87fe77bed6bab4be6895739f417cec7ad3903b
Gyorinm <sarawahida111@gmail.com>
2026-08-21 04:27:34 +0100

    feat(aethera): add textured mesh renderer interface

---
+++ include/aethera/mesh_texture_renderer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SDL.h>
+#include <cstdint>
+#include <vector>
+
+#include "aethera/image.hpp"
+#include "aethera/mesh_renderer.hpp"
+
+namespace aethera {
+
+class MeshTextureRenderer {
+public:
+    explicit MeshTextureRenderer(SDL_Renderer* renderer) : renderer_(renderer) {}
+
+    bool draw(const ImageRgba8& image, const MeshDrawData& mesh);
+
+private:
+    SDL_Renderer* renderer_{};
+};
+
+} // namespace aethera
